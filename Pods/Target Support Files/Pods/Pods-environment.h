1effdbe7bd44c7ee90268390983d75c8da7e99ca
Andrew Sardone <andrew@andrewsardone.com>
2015-03-30 18:10:16 -0400

    Remove git ignoring of Pods/, check in Pods directory
    This allows a direct clone of the repo and running, without having to
    run `pod install`. It makes git cloning to running a “pure function” in
    a way, not requiring the side-effect of pulling in other files beyond
    the repository.

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// React
+#define COCOAPODS_POD_AVAILABLE_React
+#define COCOAPODS_VERSION_MAJOR_React 0
+#define COCOAPODS_VERSION_MINOR_React 2
+#define COCOAPODS_VERSION_PATCH_React 1
+
+// React/Core
+#define COCOAPODS_POD_AVAILABLE_React_Core
+#define COCOAPODS_VERSION_MAJOR_React_Core 0
+#define COCOAPODS_VERSION_MINOR_React_Core 2
+#define COCOAPODS_VERSION_PATCH_React_Core 1
+
